622aaf2c03cbaa70405546ade5195b3a7151f645
Riley-Zlater <71993701+Riley-Zlater@users.noreply.github.com>
2020-11-17 12:05:09 -0700

    Update TrieST.java

--- outLab4/TrieST.java
+++ outLab4/TrieST.java
@@ -249,44 +249,44 @@ public class TrieST<Value> {
 		return null;
 	}
 
-	/**
-	 * Unit tests the {@code TrieST} data type.
-	 *
-	 * @param args the command-line arguments
-	 */
-	public static void main(String[] args) {
-
-		// build symbol table from standard input
-		TrieST<Integer> st = new TrieST<Integer>();
-		for (int i = 0; !StdIn.isEmpty(); i++) {
-			String key = StdIn.readString();
-			st.put(key, i);
-		}
-
-		// print results
-		if (st.size() < 100) {
-			StdOut.println("keys(\"\"):");
-			for (String key : st.keys()) {
-				StdOut.println(key + " " + st.get(key));
-			}
-			StdOut.println();
-		}
-
-		StdOut.println("longestPrefixOf(\"shellsort\"):");
-		StdOut.println(st.longestPrefixOf("shellsort"));
-		StdOut.println();
-
-		StdOut.println("longestPrefixOf(\"quicksort\"):");
-		StdOut.println(st.longestPrefixOf("quicksort"));
-		StdOut.println();
-
-		StdOut.println("keysWithPrefix(\"shor\"):");
-		for (String s : st.keysWithPrefix("shor"))
-			StdOut.println(s);
-		StdOut.println();
-
-		StdOut.println("keysThatMatch(\".he.l.\"):");
-		for (String s : st.keysThatMatch(".he.l."))
-			StdOut.println(s);
-	}
+//	/**
+//	 * Unit tests the {@code TrieST} data type.
+//	 *
+//	 * @param args the command-line arguments
+//	 */
+//	public static void main(String[] args) {
+//
+//		// build symbol table from standard input
+//		TrieST<Integer> st = new TrieST<Integer>();
+//		for (int i = 0; !StdIn.isEmpty(); i++) {
+//			String key = StdIn.readString();
+//			st.put(key, i);
+//		}
+//
+//		// print results
+//		if (st.size() < 100) {
+//			StdOut.println("keys(\"\"):");
+//			for (String key : st.keys()) {
+//				StdOut.println(key + " " + st.get(key));
+//			}
+//			StdOut.println();
+//		}
+//
+//		StdOut.println("longestPrefixOf(\"shellsort\"):");
+//		StdOut.println(st.longestPrefixOf("shellsort"));
+//		StdOut.println();
+//
+//		StdOut.println("longestPrefixOf(\"quicksort\"):");
+//		StdOut.println(st.longestPrefixOf("quicksort"));
+//		StdOut.println();
+//
+//		StdOut.println("keysWithPrefix(\"shor\"):");
+//		for (String s : st.keysWithPrefix("shor"))
+//			StdOut.println(s);
+//		StdOut.println();
+//
+//		StdOut.println("keysThatMatch(\".he.l.\"):");
+//		for (String s : st.keysThatMatch(".he.l."))
+//			StdOut.println(s);
+//	}
 }

ba856c96123e7a3138fe0e48d6a25aa74e804591
Riley-Zlater <71993701+Riley-Zlater@users.noreply.github.com>
2020-11-19 14:58:47 -0700

    Add files via upload

---
+++ outLab4/Main.java
@@ -0,0 +1,93 @@
+package outLab4;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String d_directory;
+        String input_directory;
+        
+        // try and get the input files from the cmd line
+        
+        try {
+            d_directory = args[0];
+            input_directory = args[1];
+            System.out.println("Inputted dictionary directory: " + d_directory);
+            System.out.println("Inputted input file directory: " + input_directory);
+        } catch(IndexOutOfBoundsException e) {
+            System.out.println("Invalid arguments entered.");
+            d_directory = "null";
+            input_directory = "null";
+        }
+        System.out.println("\nReading files...");
+
+        // Reads file, in hindsight the file reading and formatting 
+        // should've been done in a separate java class.
+        
+        String out_file_name = "C:\\Users\\Ara\\OneDrive\\Java_232_Projects_(OneDrive)\\Text_Files\\output.txt";
+        BufferedReader dictionary_reader;
+        BufferedReader input_reader;
+        TrieST<String> trie = new TrieST<>();
+        try {
+            dictionary_reader = new BufferedReader(new FileReader(d_directory));
+            String d_word = dictionary_reader.readLine();
+            while (d_word != null) {
+                trie.put(d_word, d_word);
+                d_word = dictionary_reader.readLine();
+            }
+            dictionary_reader.close();
+            input_reader = new BufferedReader(new FileReader(input_directory));
+            PrintWriter out_file = new PrintWriter(new FileWriter(out_file_name));
+            String line = input_reader.readLine();
+            while (line != null) {
+                String[] word_array = line.split(" ");
+                for (String word : word_array) {
+                    word = word.toLowerCase();
+                    word = word.replaceAll("[^a-zA-Z ]", "");
+                    
+                    // Search the Trie only if the word we come across is NOT already in the Trie
+                    
+                    if(!trie.contains(word)) {
+                        ArrayList<String> list = new ArrayList<>();
+                        
+                        // Use the keys with prefix method to find all the strings in our 
+                        // Trie that have similar prefixes to the misspelled word
+                        // Then add thi strings with similar prefixes to a list
+                        
+                        for(String w: trie.keysWithPrefix(word)) {
+                            list.add(w);
+                        }
+                        
+                        // if there are at least 3 words with similar prefixes 
+                        // get those words and pass all of our information through the menu
+                        
+                        if (list.size() >= 3) {
+                            String misspelled_word = word;
+                            String word1 = list.get(0);
+                            String word2 = list.get(1);
+                            String word3 = list.get(2);
+                            Menu menu = new Menu();
+                            String new_word = menu.menu(misspelled_word, word1, word2, word3);
+                            out_file.printf("%s ", new_word);
+                        } else {
+                            System.out.println(word + " not recognized. Printing as is...");
+                            out_file.printf("%s ", word);
+                        }
+
+                    } else {
+                        out_file.printf("%s ", word);
+                    }
+                }
+                out_file.printf("\n");
+                line = input_reader.readLine();
+            }
+            input_reader.close();
+            out_file.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        System.out.println("Input file corrected in output.txt.\nTerminating program...");
+    }
+}
---
+++ outLab4/Menu.java
@@ -0,0 +1,52 @@
+package outLab4;
+
+import java.util.*;
+
+public class Menu {
+
+	/**
+	 * This constructs the menu for the user. It gives them 3 words that have similar prefixes to the misspelled word
+	 * then corrects and overwrites the misspelled word.
+	 * 
+	 * @param misspelled_word String that is the word found in the file that's not found in the Trie of correct words
+	 * @param word1 String that is the first key with prefix result.
+	 * @param word2 String that is the second key with prefix result.
+	 * @param word3 String that is the third key with prefix result.
+	 * @return The corrected word as a String.
+	 */
+    public String menu(String misspelled_word, String word1, String word2, String word3) {
+        int option;
+        String new_word = "";
+        System.out.printf("\nMisspelled word detected: %s\n%s\n 1. %s\n 2. %s\n 3. %s\n 0. %s\n%s\n",
+                misspelled_word, "Did you mean: ", word1, word2, word3, "Something else.",
+                "Enter desired option below.");
+        Scanner scanner = new Scanner(System.in);
+        option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                new_word = word1;
+                System.out.println("Replacing " + misspelled_word + " with " + new_word + "...");
+                break;
+            case 2:
+                new_word = word2;
+                System.out.println("Replacing " + misspelled_word + " with " + new_word + "...");
+                break;
+            case 3:
+                new_word = word3;
+                System.out.println("Replacing " + misspelled_word + " with " + new_word + "...");
+                break;
+            case 0:
+                System.out.println("Enter desired word below.");
+                new_word = scanner.next();
+                System.out.println("Replacing " + misspelled_word + " with " + new_word + "...");
+                break;
+            default:
+                System.out.println("Invalid input detected.");
+                new_word = menu(misspelled_word, word1, word2, word3);
+                break;
+        }
+        scanner.close();
+        return new_word;   
+    }
+
+}
